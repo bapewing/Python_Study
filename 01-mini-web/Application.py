3a208bd961ba6e000cafd762662bee3133835c04
wingxy1017 <wingxy1017@163.com>
2018-03-21 00:29:13 +0800

    初步实现访问指定页面

--- 01-mini-web/Application.py
+++ 01-mini-web/Application.py
@@ -1,9 +1,36 @@
 import socket
+import re
 
 
 def handle_client_request(df_socket):
     receive_data = df_socket.recv(4096)
-    print(receive_data.decode())
+    # print(receive_data.decode().splitlines())
+    if not receive_data:
+        print("未收到请求数据,断开连接")
+        df_socket.close()
+    request_info_list = receive_data.decode().splitlines()
+    # 通过正则匹配分组获取请求的url路径
+    # TODO:偶尔会出现数组越界的问题?
+    url_resource = re.match("\w+\s+(\S+)", request_info_list[0]).group(1)
+
+    if url_resource == "/":
+        url_resource = "/index.html"
+
+    try:
+        with open("./static" + url_resource, "rb") as file:
+            response_body = file.read()
+    except Exception as e:
+        print("异常信息:%s" % e)
+        response_header = "HTTP/1.1 404 Not Found\r\n"
+        response_header += "Server:Python 1.0\r\n"
+        response_body = "Request URL Error".encode()
+    else:
+        response_header = "HTTP/1.1 200 OK\r\n"
+        response_header += "Server:Python 1.0\r\n"
+    finally:
+        response = (response_header + "\r\n").encode() + response_body
+        df_socket.send(response)
+        df_socket.close()
 
 
 def main():
