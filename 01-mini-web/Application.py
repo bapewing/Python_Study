6f21bd3c5c6fe86bcf54298237ce79521890573e
wingxy1017 <wingxy1017@163.com>
2018-03-22 00:17:23 +0800

    显示所有股票和个人收藏信息

--- 01-mini-web/Application.py
+++ 01-mini-web/Application.py
@@ -1,5 +1,6 @@
 import time
 import re
+import pymysql
 
 route_list = []
 
@@ -24,24 +25,79 @@ def get_time():
 
 @create_route_list("/index.py")
 def index():
-    data_from_sql = "暂时没有数据"
-
-    with open("./template/index.html", "r") as file:
-        html_data = file.read()
-
-    html_data = re.sub(r"\{%content%\}", data_from_sql, html_data)
-    return html_data
+    data_from_sql = ""
+    try:
+        conn = pymysql.connect(host="localhost", port=3306, db="stock_db", user="root", password="1017", charset="utf8")
+        cur = conn.cursor()
+        sql = "SELECT * FROM info"
+        cur.execute(sql)
+        for line in cur.fetchall():
+            line_data = """
+            <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td><input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="%s"></td>
+        </tr>
+            """ % (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[1])
+            data_from_sql += line_data
+        conn.commit()
+        with open("./template/center.html", "r") as file:
+            html_data = file.read()
+    except Exception as e:
+        conn.rollback()
+        return "获取数据失败" + str(e)
+    else:
+        html_data = re.sub(r"\{%content%\}", data_from_sql, html_data)
+        return html_data
+    finally:
+        cur.close()
+        conn.close()
 
 
 @create_route_list("/center.py")
 def center():
-    data_from_sql = "暂时没有数据"
-
-    with open("./template/center.html", "r") as file:
-        html_data = file.read()
-
-    html_data = re.sub(r"\{%content%\}", data_from_sql, html_data)
-    return html_data
+    data_from_sql = ""
+    try:
+        conn = pymysql.connect(host="localhost", port=3306, db="stock_db", user="root", password="1017", charset="utf8")
+        cur = conn.cursor()
+        sql = "SELECT i.code,i.short,i.chg,i.turnover,i.price,i.highs,f.note_info FROM info AS i INNER JOIN focus AS f ON i.id = f.info_id"
+        cur.execute(sql)
+        for line in cur.fetchall():
+            line_data = """
+              <tr>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>%s</td>
+                    <td>
+                        <a type="button" class="btn btn-default btn-xs" href="/update/%s.html"> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> 修改 </a>
+                    </td>
+                    <td>
+                        <input type="button" value="删除" id="toDel" name="toDel" systemidvaule="%s">
+                    </td>
+                </tr>
+              """ % (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[0], line[0])
+            data_from_sql += line_data
+        conn.commit()
+        with open("./template/center.html", "r") as file:
+            html_data = file.read()
+    except Exception as e:
+        return "获取数据失败" + str(e)
+    else:
+        html_data = re.sub(r"\{%content%\}", data_from_sql, html_data)
+        return html_data
+    finally:
+        cur.close()
+        conn.close()
 
 
 # django添加路由列表
