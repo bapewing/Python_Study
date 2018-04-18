c0d77ec00b78649e7887d9866e91fb996e81902b
wingxy1017 <wingxy1017@163.com>
2018-04-18 20:14:35 +0800

    设置session保存位置

--- 02-flask_project/manager.py
+++ 02-flask_project/manager.py
@@ -2,17 +2,26 @@ import flask
 import flask_sqlalchemy
 import flask_wtf
 import redis
+import flask_session
 
 
 # 添加项目相关配置
 class Config(object):
     DEBUG = True
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
     # mysql配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:1017@127.0.0.1:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # redis配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启session标签
+    SESSION_USE_SIGNER = True
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = 86400 * 3
 
 
 app = flask.Flask(__name__)
@@ -21,10 +30,13 @@ info_db = flask_sqlalchemy.SQLAlchemy(app)
 # 只做服务器验证工作，cookie中的 csrf_token 和表单中的 csrf_token 需要我们自己实现
 flask_wtf.csrf.CSRFProtect(app)
 redis_db = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+flask_session.Session(app)
 
 
 @app.route("/")
 def index():
+    flask.session['name'] = "wxy"
+    flask.session['age'] = 18
     return "index"
 
 
