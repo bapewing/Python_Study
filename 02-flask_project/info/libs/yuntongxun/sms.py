d752ebf6026107e9f5af2e8178c923ba4c7b6b26
wingxy1017 <wingxy1017@163.com>
2018-04-20 20:21:26 +0800

    短信验证码后端逻辑实现

--- 02-flask_project/info/libs/yuntongxun/sms.py
+++ 02-flask_project/info/libs/yuntongxun/sms.py
@@ -1,15 +1,15 @@
 # -*- coding:utf-8 -*-
 
-from .CCPRestSDK import REST
+from info.libs.yuntongxun.CCPRestSDK import REST
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8aaf0708568d4143015697b0f4960888'
+_accountSid = '8a216da862dcd1050162e2d3df820462'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '57c6c3ef3cef47e680519a734f6812f8'
+_accountToken = 'ba6e75b56b474b2981793ca9cfea7894'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8aaf0708568d4143015697b0f56e088f'
+_appId = '8a216da862dcd1050162e2d3dfe60469'
 
 # 说明：请求地址，生产环境配置成app.cloopen.com
 _serverIP = 'sandboxapp.cloopen.com'
@@ -72,4 +72,4 @@ class CCP(object):
 if __name__ == '__main__':
     ccp = CCP()
     # 注意： 测试的短信模板编号为1
-    ccp.send_template_sms('18516952650', ['1234', 5], 1)
+    ccp.send_template_sms('17796969160', ['1234', 5], 1)
--- 02-flask_project/info/modules/passport/views.py
+++ 02-flask_project/info/modules/passport/views.py
@@ -1,8 +1,13 @@
+import random
+import re
+
 import flask
 
 from info import redis_db
 from info.utils import constants
 from info.utils.captcha.captcha import captcha
+from info.utils.response_code import RET
+from sms import CCP
 from . import passport_blu
 
 
@@ -36,3 +41,55 @@ def get_image_code():
         # 兼容所有浏览器
         response.headers['Content-Type'] = "image/jpg"
         return response
+
+
+@passport_blu.route('/sms_code')
+def send_sms_code(errmsg=None):
+    """
+    发送短信的逻辑
+    1. 获取参数：手机号，图片验证码内容，图片验证码的编号 (随机值)
+    2. 校验参数(参数是否符合规则，判断是否有值)
+    3. 先从redis中取出真实的验证码内容
+    4. 与用户的验证码内容进行对比，如果对比不一致，那么返回验证码输入错误
+    5. 如果一致，生成验证码的内容(随机数据)
+    6. 发送短信验证码,并保存验证码，以便校验
+    7. 告知发送结果
+    """
+    # 本质：最后调用loads函数
+    parameters = flask.request.json
+    # parameters = flask.json.loads(flask.request.data)
+    mobile_phone = parameters.get('mobile')
+    image_code = parameters.get('image_code')
+    image_code_id = parameters.get('image_code_id')
+
+    if not all([mobile_phone, image_code, image_code_id]):
+        return flask.jsonify(errno=RET.PARAMERR, errmsg='参数有误')
+    if not re.match('1[35678]\\d{9}', mobile_phone):
+        return flask.jsonify(errno=RET.PARAMERR, errmsg='手机号格式不正确')
+
+    try:
+        real_image_code = redis_db.get('imageCodeId' + image_code_id)
+    except Exception as e:
+        flask.current_app.logger(e)
+        return flask.jsonify(errno=RET.DBERR, errmsg='数据查询失败')
+    else:
+        # 过期
+        if not real_image_code:
+            return flask.jsonify(errno=RET.NODATA, errmsg='图片验证码过期')
+        if image_code.upper() != real_image_code:
+            return flask.jsonify(error=RET.DATAERR, errmsg='输入验证码错误')
+        # 验证通过，可以发送手机验证码了
+        sms_code_str = "%06d" % random.randint(0, 999999)
+        flask.current_app.logger.debug("短信验证码内容是：%s" % sms_code_str)
+        result = CCP().send_template_sms(mobile_phone, sms_code_str, constants.SMS_CODE_REDIS_EXPIRES/5, '1')
+
+        if result != 0:
+            return flask.jsonify(errno=RET.THIRDERR, errmsg='发送短信失败')
+        else:
+            try:
+                # 手机号作为key
+                redis_db.set('SMS_' + mobile_phone, sms_code_str, constants.SMS_CODE_REDIS_EXPIRES)
+            except Exception as e:
+                flask.current_app.logger(e)
+                return flask.jsonify(errno=RET.DBERR, errmsg='数据保存失败')
+            return flask.jsonify(errno=RET.OK, errmsg='发送成功')
