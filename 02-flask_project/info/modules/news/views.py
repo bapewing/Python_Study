ca44fc4feaa579dba4ae7966314e458a6fd5457e
wingxy1017 <wingxy1017@163.com>
2018-04-23 17:28:00 +0800

    首页和详情页模板抽取基类

--- 02-flask_project/info/modules/news/views.py
+++ 02-flask_project/info/modules/news/views.py
@@ -5,4 +5,7 @@ from . import news_blu
 
 @news_blu.route('/<int:news_id>')
 def news_detail(news_id):
-    return flask.render_template('news/detail.html')
+    data = {
+
+    }
+    return flask.render_template('news/detail.html', data=data)
