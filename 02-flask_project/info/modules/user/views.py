efcbf77cc60bb62b8b9f31b41bc74c1dbe0bbe62
wingxy1017 <wingxy1017@163.com>
2018-04-24 17:39:54 +0800

    基本资料界展示

--- 02-flask_project/info/modules/user/views.py
+++ 02-flask_project/info/modules/user/views.py
@@ -15,3 +15,10 @@ def user_info():
         'user': user.to_dict()
     }
     return flask.render_template('user/user.html', data=data)
+
+
+@user_blu.route('/user_base_info', methods=['GET', 'POST'])
+def user_base_info():
+    if flask.request.method == "GET":
+        return flask.render_template('user/user_base_info.html')
+
