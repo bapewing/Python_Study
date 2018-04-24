748e649ae1e474db6dfb515c4cb12a163179eaa2
wingxy1017 <wingxy1017@163.com>
2018-04-24 17:01:46 +0800

    个人中心模块及模板基类抽取

--- 02-flask_project/info/__init__.py
+++ 02-flask_project/info/__init__.py
@@ -70,6 +70,8 @@ def create_app(config_pattern):
     app.register_blueprint(passport_blu)
     from info.modules.news import news_blu
     app.register_blueprint(news_blu)
+    from info.modules.user import user_blu
+    app.register_blueprint(user_blu)
 
     # 其实也可以返回元组 app, info_db
     return app
---
+++ 02-flask_project/info/modules/user/views.py
@@ -0,0 +1,17 @@
+import flask
+
+from info.utils.common import user_login_data
+from . import user_blu
+
+
+@user_blu.route('/user_info')
+@user_login_data
+def user_info():
+    user = flask.g.user
+    if not user:
+        # return flask.jsonify(errno=RET.SESSIONERR, errmsg='用户未登录')
+        return flask.redirect('/')
+    data = {
+        'user': user.to_dict()
+    }
+    return flask.render_template('user/user.html', data=data)
